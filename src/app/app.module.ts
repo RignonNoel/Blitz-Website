9baaf3a5bb84987d94acd950b19062ac3bbe98f3
RignonNoel <rignon.noel@gmail.com>
2018-04-19 18:26:51 -0400

    POC angular-calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
+import { CalendarModule } from 'angular-calendar';
 
 import { AppComponent } from './app.component';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
+
+const appRoutes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomePageComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultLayoutComponent,
+    HeaderComponent,
+    HomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
